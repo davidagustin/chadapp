4346a4ee8cef772a600fd683108b301fa8cbaf6d
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-02-21 14:26:47 -0800

    Creating skeleton for Onboard Screen and Styling

---
+++ app/components/screens/Onboard/Onboard.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+/* eslint-disable space-before-function-paren */
+
+// packages
+import React from 'react'
+import { Text, View } from 'react-native'
+
+// atoms
+
+
+// templates
+import { Layout, Wrapper } from '../../templates'
+
+// modules
+import { size } from '../../../utilities'
+
+// styles
+import screen from '../base/Screen.style'
+import ads_onboard from './Onboard.style'
+
+export default class OnboardScreen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+  }
+
+  render = () => {
+    return (
+      <Layout gradient>
+        <Wrapper keykeyboard style={[screen.wrapper]}>
+          
+        </Wrapper>
+      </Layout>
+    )
+  }
+}
+
---
+++ app/components/screens/Onboard/Onboard.style.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+/* eslint-disable space-before-function-paren */
+
+// packages
+import { StyleSheet } from 'react-native'
+
+// modules
+import {} from '../../atoms'
+import { size, percentage } from '../../../utilities'
+
+const ads_onboard = StyleSheet.create({
+
+})
+
+export default ads_onboard
