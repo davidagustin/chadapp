2a1f5121a5a9f45a37f0d72c79adc2b6f77ead7e
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-02-22 18:00:50 -0800

    First screen set up, working on animations, waiting on images

--- app/components/screens/Onboard/Onboard.js
+++ app/components/screens/Onboard/Onboard.js
@@ -3,10 +3,10 @@
 
 // packages
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Animated, TouchableOpacity, Image } from 'react-native'
 
 // atoms
-
+import { Button } from '../../atoms'
 
 // templates
 import { Layout, Wrapper } from '../../templates'
@@ -21,22 +21,69 @@ import ads_onboard from './Onboard.style'
 export default class OnboardScreen extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      click: 1
+    }
+
+    // all items that will have animations
+    this.getStartedButtonAnimation = new Animated.ValueXY({ x: 12, y: -250 })
+  }
+
+  handlePress = click => {
+    // do animation based on click number
+    console.log('click')
+    if (click === 1) {
+      this.firstPressAnimations()
+    } else if (click === 2) {
+      this.secondPressAnimations()
+    } else {
+      this.thirdPressAnimations()
+    }
+    // this.setState({
+    //   click: this.state.click += 1
+    // })
+  }
+
+  firstPressAnimations = () => {
+    // Button Animation
+    Animated.spring(this.getStartedButtonAnimation, {
+      toValue: {x: 50, y: -10},
+    }).start() 
+  }
+
+  secondPressAnimations = () => {
 
   }
 
-  render = () => {
+  thirdPressAnimations = () => {
+
+  }
+
+  render () {
     return (
       <Layout gradient>
-        <Wrapper keykeyboard style={[screen.wrapper]}>
-          <View >
-            <Text >
-              Onboard Screen
-            </Text>
+        <Wrapper style={[screen.wrapper]}>
+          <View style={screen.div}>
+          <Image
+          style={ads_onboard.chadLogo}
+          source={require('../../../assets/images/smallerLogo.png')}
+        />
+            <Text style={ads_onboard.heading}>CHAD</Text>
+            <Text style={ads_onboard.subheading}>Connect with others through</Text>
+            <Text style={ads_onboard.subheading}>location based chatrooms.</Text>
+            <Animated.View style={[ads_onboard.btn_wrapper, this.getStartedButtonAnimation.getLayout()]}>
+              <TouchableOpacity style={ads_onboard.btn} onPress={this.handlePress()}>
+                <Text style={ads_onboard.btn_text}>
+                  Get Started
+                </Text>
+              </TouchableOpacity>
+            </Animated.View>
           </View>
         </Wrapper>
       </Layout>
     )
   }
 }
+
+
 
--- app/components/screens/Onboard/Onboard.style.js
+++ app/components/screens/Onboard/Onboard.style.js
@@ -6,21 +6,62 @@ import { StyleSheet } from 'react-native'
 
 // modules
 import { 
-  PURPLE, GREEN, BASE 
+  PURPLE, GREEN, BASE, FONT_XXXXL, FONT_LARGE, FONT_MEDIUM, FONT_SMALL
 } from '../../atoms'
 import { size, percentage } from '../../../utilities'
 
 const ads_onboard = StyleSheet.create({
+  div: {
+    display: 'flex',
+    alignItems: 'center',
+    height: percentage(100),
+    width: percentage(100),
+     
+  },
+  chadLogo: {
+    marginTop: size(200),
+    width: 100,
+    height: 100
+  },
+  heading: {
+    color: BASE.light,
+    fontSize: FONT_XXXXL,
+    fontWeight: '300',
+    marginBottom: size(15),
+    marginTop: size(85),
+    textAlign: 'center'
+  },
+  subheading: {
+    color: BASE.light,
+    fontSize: FONT_LARGE,
+    fontWeight: '300',
+    textAlign: 'center',
+    width: percentage(90)
+  },
   btn: {
     backgroundColor: '#41f4d0',
     borderRadius: size(25),
     height: size(60),
-    marginBottom: size(10),
-    marginTop: size(10),
     width: size(260)
   }, 
   btn_text: {
-    color: BASE.light
+    color: BASE.light,
+    fontSize: size(25),
+    textAlign: 'center',
+    marginTop: size(13)
+  },
+  btn_wrapper: {
+    height: percentage(90),
+    padding: size(20),
+    width: percentage(100),
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 5
   }
 })
 
