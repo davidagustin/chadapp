133321e595bc70bc1e21ad431aa37fc158632215
Jacob  Metzinger <jacobmetzinger@Jacobs-MBP.socal.rr.com>
2019-02-25 19:26:59 -0800

    Have fading animations working now

--- app/components/screens/Onboard/Onboard.js
+++ app/components/screens/Onboard/Onboard.js
@@ -6,13 +6,11 @@ import React from 'react'
 import { Text, View, Animated, TouchableOpacity, Image } from 'react-native'
 
 // atoms
-import { Button } from '../../atoms'
+// maybe change some animations into atoms?
+import { } from '../../atoms'
 
 // templates
 import { Layout, Wrapper } from '../../templates'
-
-// modules
-import { size } from '../../../utilities'
 
 // styles
 import screen from '../base/Screen.style'
@@ -22,19 +20,27 @@ export default class OnboardScreen extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      click: 1
+      click: 1,
+      fadeOutAnimation: new Animated.Value(1),
+      fadeInAnimation: new Animated.Value(0)
     }
 
     // all items that will have animations
 
-    // Starting Positions for Translating Animations
-    this.getStartedButton = new Animated.ValueXY({ x: 12, y: 280 })
-    this.chadLogo = new Animated.ValueXY({x: 8, y: 135}) 
-    this.chadHeading = new Animated.ValueXY({x: 8, y: 150})
+    // Starting Positions for Items Receiving Translating Animations
+    this.getStartedButton = new Animated.ValueXY({ x: 12, y: 0 })
+    this.chadLogo = new Animated.ValueXY({ x: 0, y: -150 }) 
+    this.chadHeading = new Animated.ValueXY({ x: 8, y: -130 })
+    this.progress_bar_wrapper = new Animated.ValueXY({ x: 12, y: 240 })
+    this.progress_bar_inner = new Animated.ValueXY({ x: -20, y: 240 })
+    this.locationLogo = new Animated.ValueXY({x: 300, y: -190})
+    //this.chatRoomLogo = new Animated.ValueXY({x: -600, y: 200})
+    //this.speakerLogo = new Animated.ValueXY({x: 8, y: -100})
+    //this.audioWaves = new Animated.ValueXY({x: 500, y: 200})
 
 
-    // Starting Values for Fading/Resizing Animations
-
+    // Starting Values for Items Receiving Fading/Resizing Animations
+    
   }
 
   // static navigationOptions = {
@@ -65,20 +71,33 @@ export default class OnboardScreen extends React.Component {
   firstForwardPressAnimations = () => {
     // Moving Get Started Button Off Screen
     Animated.timing(this.getStartedButton, {
-      toValue: {x: 12, y: 500},
+      toValue: {x: 12, y: 800},
       duration: 800
     }).start() 
 
     // Moving Chad Logo to New Position
     Animated.timing(this.chadLogo, {
-      toValue: {x: -50, y: 200},
+      toValue: {x: -80, y: -70},
+      duration: 800
+    }).start()
+
+    // Moving Location Logo Onto Screen
+    Animated.timing(this.locationLogo, {
+      toValue: {x: 100, y: -190},
       duration: 800
     }).start()
 
     // Moving Chad Header and Resizing
     Animated.timing(this.chadHeading, {
-      toValue: {x: 8, y: 50},
+      toValue: {x: 8, y: -480},
       duration: 800
+    }).start()
+
+    //Fade Background Circle Away
+    Animated.timing(this.state.fadeAnimation, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
     }).start()
   }
 
@@ -93,17 +112,26 @@ export default class OnboardScreen extends React.Component {
 
 
   render () {
+    let { fadeOutAnimation, fadeInAnimation } = this.state
+
     return (
       <Layout gradient>
         <Wrapper style={[screen.wrapper]}>
           <View style={screen.div}>
-          <Animated.Image
-          style={[ads_onboard.chadLogo, this.chadLogo.getLayout()]}
-          source={require('../../../assets/images/Speaker.png')}
-          />
+            <Animated.View style={[ads_onboard.circle_logo_surrounding, { opacity: fadeOutAnimation}]}></Animated.View>
+            <Animated.Image
+            style={[ads_onboard.chadLogo, this.chadLogo.getLayout()]}
+            source={require('../../../assets/images/logoOnboard.png')}
+            />
+            <Animated.Image 
+            style={[ads_onboard.location_logo, this.locationLogo.getLayout()]}
+            source={require('../../../assets/images/LocationSymbol.png')}
+            />
             <Animated.Text style={[ads_onboard.heading, this.chadHeading.getLayout()]}>CHAD</Animated.Text>
-            <Text style={ads_onboard.subheading}>Connect with others through</Text>
-            <Text style={ads_onboard.subheading}>location based chatrooms.</Text>
+            <Animated.View style={{opacity: fadeOutAnimation}}>
+              <Text style={ads_onboard.subheading}>Connect with others through</Text>
+              <Text style={ads_onboard.subheading}>location based chatrooms.</Text>
+            </Animated.View>
             <Animated.View style={[ads_onboard.btn_wrapper, this.getStartedButton.getLayout()]}>
               <TouchableOpacity style={ads_onboard.btn} onPress={this.handleForwardPress}>
                 <Text style={ads_onboard.btn_text}>
@@ -111,6 +139,8 @@ export default class OnboardScreen extends React.Component {
                 </Text>
               </TouchableOpacity>
             </Animated.View>
+            <View style={ads_onboard.progress_bar_wrapper}></View>
+            <View style={ads_onboard.progress_bar_inner}></View>
             
           </View>
         </Wrapper>
--- app/components/screens/Onboard/Onboard.style.js
+++ app/components/screens/Onboard/Onboard.style.js
@@ -19,8 +19,42 @@ const ads_onboard = StyleSheet.create({
      
   },
   chadLogo: {
+    width: 120,
+    height: 120
+  },
+  location_logo: {
+    width: 120,
+    height: 120
+  },
+  chatroom_logo: {
     width: 100,
     height: 100
+  },
+  speaker_logo: {
+    width: 100,
+    height: 100
+  },
+  audio_waves: {
+    width: 120,
+    height: 120
+  },
+  circle_logo_surrounding: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    backgroundColor: '#7D59DC', 
+    marginTop: size(68),
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 5
+  },
+  circle_button_surrounding: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: '#41f4d0', 
   },
   heading: {
     color: BASE.light,
@@ -33,7 +67,8 @@ const ads_onboard = StyleSheet.create({
     fontSize: FONT_LARGE,
     fontWeight: '300',
     textAlign: 'center',
-    width: percentage(90)
+    width: percentage(90),
+    marginTop: size(),
   },
   btn: {
     backgroundColor: '#41f4d0',
@@ -59,6 +94,25 @@ const ads_onboard = StyleSheet.create({
     shadowRadius: 2,  
     elevation: 5
   },
+  progress_bar_wrapper: {
+    backgroundColor: '#41f4d0',
+    borderRadius: size(20),
+    height: size(25),
+    width: size(279),
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 5
+  },
+  progress_bar_inner: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: size(20),
+    height: size(25),
+    width: size(93),
+    marginTop: size(-25),
+    marginRight: size(185)
+  }
 })
 
 export default ads_onboard
